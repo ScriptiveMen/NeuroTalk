8fa6fc225442dfafbe1b4d76785ec94d33ee334b
ScriptiveMen <i.satyakumarram@gmail.com>
2025-08-13 02:16:29 +0530

    feat: implement user registration functionality

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const indexRoutes = require("./routes/index.routes");
 const authRoutes = require("./routes/auth.routes");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -8,6 +9,7 @@ app.use(express.static("public"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/", indexRoutes);
 app.use("/auth", authRoutes);
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,39 @@
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 async function getRegisterController(req, res) {
   res.render("register");
 }
 
-async function postRegisterController(req, res) {}
+async function postRegisterController(req, res) {
+  const { username, email, password } = req.body;
+  const isUserExist = await userModel.findOne({
+    $or: [{ username: username }, { email: email }],
+  });
+
+  if (isUserExist) {
+    return res.status(409).json({
+      message: "User already exists!",
+    });
+  }
+
+  const hashedpassword = await bcrypt.hash(password, 10);
+
+  const user = await userModel.create({
+    username: username,
+    email: email,
+    password: hashedpassword,
+  });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User registered sucessfully",
+    user,
+  });
+}
 
 async function getLoginController(req, res) {
   res.render("login");
